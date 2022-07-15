78b12d6703402c7cdda0c6e9d9a6b3c5314b7358
caiolaytynher <caiolaytynherbr@gmail.com>
2022-07-15 16:00:31 -0300

    added more usecases.

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,18 +1,64 @@
+import re
 from typing import Pattern
 
 
 def change_line(
     filepath: str,
-    target_regex: Pattern[str],
-    replacement_text: str,
+    target: str,
+    replacement: str,
 ) -> None:
+    pattern: Pattern[str] = re.compile(target)
+
     with open(filepath, "r") as file:
         file_string = "".join(file.readlines())
 
-    matches = target_regex.finditer(file_string)
-    for match in matches:
-        start, end = match.span()
-        file_string = file_string.replace(file_string[start:end], replacement_text)
+    modified_file_string = pattern.sub(replacement, file_string)
 
     with open(filepath, "w") as file:
-        file.write(file_string)
+        file.write(modified_file_string)
+
+
+def change_lines(
+    filepath: str,
+    targets: list[str],
+    replacements: list[str],
+) -> None:
+    if len(targets) != len(replacements):
+        raise Exception("The targets and the replacements should have the same size.")
+
+    patterns: list[Pattern[str]] = [re.compile(target) for target in targets]
+
+    with open(filepath, "r") as file:
+        file_string = "".join(file.readlines())
+
+    modified_file_string = file_string
+    for (
+        pattern,
+        replacement,
+    ) in zip(patterns, replacements):
+        modified_file_string = pattern.sub(replacement, modified_file_string)
+
+    with open(filepath, "w") as file:
+        file.write(modified_file_string)
+
+
+def change_template(
+    input_filepath: str,
+    targets: list[str],
+    replacements: list[str],
+    output_filepath: str,
+) -> None:
+    if len(targets) != len(replacements):
+        raise Exception("The targets and the replacements should have the same size.")
+
+    patterns: list[Pattern[str]] = [re.compile(target) for target in targets]
+
+    with open(input_filepath, "r") as file:
+        file_string = "".join(file.readlines())
+
+    modified_file_string = file_string
+    for pattern, replacement in zip(patterns, replacements):
+        modified_file_string = pattern.sub(replacement, modified_file_string)
+
+    with open(output_filepath, "w") as file:
+        file.write(modified_file_string)
