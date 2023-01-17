5c11a82a107cdd7067c15318464a52fdaae1598e
caiolaytynher <caiolaytynherbr@gmail.com>
2023-01-17 19:11:05 -0300

    created themes module to substitute colors

---
+++ themes/theme.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Theme:
+    name: str
+    background: str
+    contrast: list[str]  # higher index -> lighter color
+    foreground: str
+    wallpaper: str
+    accent: str
---
+++ themes/themes.py
@@ -0,0 +1,47 @@
+from .theme import Theme
+
+
+gruvbox = Theme(
+    name="gruvbox",
+    background="#282828",
+    contrast=[
+        "#3c3836",
+        "#504945",
+        "#665c54",
+        "#7c6f64",
+    ],
+    foreground="#ebdbb2",
+    wallpaper="serenity-1920x1080.jpg",
+    accent="#d65d0e",  # Orange
+    # black="#282828"
+    # red="#cc241d"
+    # green="#98971a"
+    # yellow='#d79921'
+    # orange="#d65d0e"
+    # blue="#458588"
+    # magenta="#b16286"
+    # cyan="#689d6a"
+    # white="#a89984"
+)
+
+dracula = Theme(
+    name="dracula",
+    background="#282a36",
+    contrast=[
+        "#343746",
+        "#424450",
+        "#535560",
+        "#63646e",
+    ],
+    foreground="#f8f8f2",
+    wallpaper="blue-landscape.jpg",
+    accent="#bd93f9",  # Blue
+    # black="#21222c",
+    # red="#ff5555",
+    # green="#50fa7b",
+    # yellow="#f1fa8c",
+    # blue="#bd93f9",
+    # magenta="#ff79c6",
+    # cyan="#8be9fd",
+    # white="#f8f8f2",
+)
