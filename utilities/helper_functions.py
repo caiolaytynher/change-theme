fc64e9974c60dbe6bd9be79c792e04abcb0a3e72
caiolaytynher <caiolaytynherbr@gmail.com>
2023-01-17 19:09:30 -0300

    removed utilities module

--- utilities/helper_functions.py
+++
@@ -1,8 +0,0 @@
-def get_file_str(filepath: str) -> str:
-    with open(filepath, "r") as file:
-        return "".join(file.readlines())
-
-
-def update_file(filepath: str, content: str) -> None:
-    with open(filepath, "w") as file:
-        file.write(content)
